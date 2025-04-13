13b27435cdba7a54e4dfe2f66988e2fb0bbadb49
Goroza7 <nikagorozia7@gmail.com>
2025-04-13 05:29:56 +0400

    commit

---
+++ DevOps/Tests/test.py
@@ -0,0 +1,24 @@
+import unittest
+from hello import add, subtract, multiply, divide
+
+
+class TestMathFunctions(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(5, 10), -5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(-2, 3), -6)
+        self.assertEqual(multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertAlmostEqual(divide(10, 2), 5)
+        self.assertAlmostEqual(divide(3, 2), 1.5)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
